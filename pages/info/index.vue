1685bbe13e44067fbc3b58725befa0a2ea388288
sotarokanai <sotarokanai@gmail.com>
2022-06-09 20:16:51 +0900

    add nuxt.js tool

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -4,7 +4,7 @@
             <ttlTxt>profile</ttlTxt>
             <paragraph>名前: 金井 奏太朗　カナイ ソウタロウ<br>出身: 群馬<br>年齢: 20<br>学校: 獨協大学 経済学部経済学科 在籍中<br>　　&nbsp;&nbsp;桑沢デザイン研究所 専攻デザイン科 ビジュアルデザイン専攻 在籍中<br>趣味: 散歩<br>愛犬: <a href="https://www.instagram.com/mamimi_inu/">まみみ</a></paragraph>
              <ttlTxt>tool</ttlTxt>
-            <paragraph>Touch Designer, AxiDraw, Adobe: Illustrator Photoshop, ファイバーレーザーマーカー（光響製）</paragraph>
+            <paragraph>Touch Designer, AxiDraw, Adobe: Illustrator Photoshop, ファイバーレーザーマーカー（光響製）,Nuxt.js</paragraph>
             <!-- default_size="800px"ここでサイズ指定できる
             srcの中に好きな画像入れて -->
             <contentImage
